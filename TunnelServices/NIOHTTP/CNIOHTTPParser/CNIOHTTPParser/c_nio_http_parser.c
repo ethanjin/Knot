97f29607c47b782c7a12b8774134bdf9841f1cdf
yichenjin <yichenjin@cmbchina.com>
2020-04-01 10:27:00 +0800

    解决头文件引用

--- TunnelServices/NIOHTTP/CNIOHTTPParser/CNIOHTTPParser/c_nio_http_parser.c
+++ TunnelServices/NIOHTTP/CNIOHTTPParser/CNIOHTTPParser/c_nio_http_parser.c
@@ -21,7 +21,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include "include/c_nio_http_parser.h"
+#include "c_nio_http_parser.h"
 #include <assert.h>
 #include <stddef.h>
 #include <ctype.h>
